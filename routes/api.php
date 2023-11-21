53647ca398e393d3646a2c700c3e352e2d2501c2
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-11-21 10:49:17 -0300

    feat: EX03- Markers finalizado

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
+
+use App\Http\Controllers\ProductMarkerController;
 use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
+Route::get('markers', [MarkerController::class, 'index']);
+Route::post('markers', [MarkerController::class, 'store']);
 
+Route::get('product_markers', [ProductMarkerController::class, 'index']);
+Route::post('product_markers', [ProductMarkerController::class, 'store']);
