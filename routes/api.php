529d848f0039d39a713def610795c73763efb38b
natanaelbguilherme <natanaelbguilherme@gmail.com>
2023-11-27 19:26:59 -0300

    feat add rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
+
+use App\Http\Controllers\AssetController;
 use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
-
+Route::get('asset', [AssetController::class, 'index']);
+Route::post('asset', [AssetController::class, 'store']);
+Route::put('asset{id}', [AssetController::class, 'update']);
+Route::delete('asset{id}', [AssetController::class, 'destroy']);
